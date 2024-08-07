b83667ce62bcf9c052c4377ad211d8d15be8b6e3
xmxdeus <119981982+xmxdeusltd@users.noreply.github.com>
2024-08-07 21:32:21 +0100

    added wallet, restructured project

---
+++ web/app/components/WalletProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+export default function WalletProviderComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
---
+++ web/app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+import SolanaButton from "./components/SolanaButton";
+
+// not the most optimal way but fixes hydration errors
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+export default function Home() {
+  // design is minimal purely for demo purposes
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <nav className="fixed top-0 left-0 right-0 flex justify-between items-center p-4 text-white">
+        <h1 className="text-xl font-bold">Staker</h1>
+        <WalletMultiButtonDynamic />
+      </nav>
+      <div className="relative border p-2 rounded-lg w-full max-w-md flex-col flex gap-4 place-items-center justify-center">
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold text-center">Stake</h1>
+          <p className="text-sm text-gray-500">
+            Stake your SOL to earn rewards
+          </p>
+        </div>
+        <SolanaButton>Greet</SolanaButton>
+      </div>
+    </main>
+  );
+}
